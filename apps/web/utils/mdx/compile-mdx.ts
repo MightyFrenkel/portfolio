edfe0ffad441e8d62d991fcc172b69d0a914c614
Frank Hartman <actuallyfrank@pm.me>
2023-09-16 12:07:06 +0200

    fix: hoisted
    Signed-off-by: Frank Hartman <actuallyfrank@pm.me>

--- apps/web/utils/mdx/compile-mdx.ts
+++ apps/web/utils/mdx/compile-mdx.ts
@@ -14,6 +14,6 @@ export async function getMdx(source: string) {
         remarkPlugins: [remarkGfm],
       },
     },
-    components: ComponentsConfig as any,
+    components: ComponentsConfig,
   });
 }
